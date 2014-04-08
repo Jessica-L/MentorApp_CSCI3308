d0d62d3a52f1558292890dc4901bca249b1bd8ac
saasbook <saasbook@saasbook.(none)>
2014-04-08 18:20:07 -0300

    added gitignore

---
+++ features/step_definitions/users_steps.rb
@@ -0,0 +1,15 @@
+Given /user does not have a profile$/ do
+  pending
+end
+
+And /they do not have information in the database$/ do
+  pending
+end
+
+And /they hit new profile and fill out their information$/ do
+  pending
+end
+
+Then /they should see their profile page$/ do
+  pending
+end
