babac044d136e0dcf045a6bc1a76f8b0bb9cc4a9
TheNoar <noah.dillon@colorado.edu>
2014-04-09 17:28:04 -0600

    noah fixes calendar

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140408035736) do
+ActiveRecord::Schema.define(:version => 20140409222214) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
     t.datetime "start_at"
     t.datetime "end_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
